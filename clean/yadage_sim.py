a537e6792abd8b7e9dd4e4ca673b9cc1538c576b
lukasheinrich <lukas.heinrich@gmail.com>
2017-06-08 14:42:05 +0200

    generic-ish yadage

--- clean/yadage_sim.py
+++ clean/yadage_sim.py
@@ -11,23 +11,31 @@ import tempfile
 logging.getLogger('adage').setLevel(logging.ERROR)
 logging.getLogger('yadage').setLevel(logging.ERROR)
 
+def pars_to_initdata(theta,phi,n_samples):
+    theta, phi, n_samples = float(theta), float(phi), int(n_samples)
+    return {'n_samples':n_samples,'phi': phi,'theta':theta}
+
+def workflow_config(initdata):
+    return ['workflow.yml','../workflows/codes',initdata]
+
+def load_data(adageobj):
+    return np.load(adageobj.view().getSteps('generate')[0].result['outfile'])
+
 def simulator(theta,phi,n_samples, trackers = []):
     theta, phi, n_samples = float(theta), float(phi), int(n_samples)
     sys.stdout.write('.')
     workdir = tempfile.mkdtemp(dir = os.environ.get('YCOMB_WOKRKDIR_BASE',os.path.abspath(os.curdir)))
     ys = yadage.steering_object.YadageSteering()
     ys.prepare_workdir(workdir)
-    initdata ={'n_samples':n_samples,'phi': phi,'theta':theta}
+    initdata = pars_to_initdata(theta,phi,n_samples)
     ys.init_workflow(
-        'workflow.yml',
-        '../workflows/codes',
-        initdata
+        *workflow_config(initdata)
     )
 
     backend = setupbackend_fromstring(os.environ.get('YCOMB_BACKEND','multiproc:4'))
     ys.adage_argument(additional_trackers = trackers, default_trackers = False)
     ys.run_adage(backend)
 
-    data = np.load(ys.controller.adageobj.view().getSteps('generate')[0].result['outfile'])
+    data = load_data(ys.controller.adageobj)
     shutil.rmtree(workdir)
     return data
