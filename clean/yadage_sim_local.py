a01d1b500a504578918ed1179625322f5d2faff5
lukasheinrich <lukas.heinrich@gmail.com>
2017-06-09 10:02:41 +0200

    up

--- clean/yadage_sim_local.py
+++ clean/yadage_sim_local.py
@@ -38,5 +38,5 @@ def simulator(theta,phi,n_samples, widget = None, delay = False):
     ys.run_adage(backend)
 
     data = np.load(open(ys.controller.adageobj.view().getSteps('feature_extraction')[0].result['outfile']))
-    shutil.rmtree(workdir)
+    shutil.rmtree(workdir)l
     return data
