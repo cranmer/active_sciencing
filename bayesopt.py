50bb6479bf9029067dc821876b61680cf0862378
lukasheinrich <lukas.heinrich@gmail.com>
2017-06-10 02:38:03 +0200

    kernel tweaks

--- bayesopt.py
+++ bayesopt.py
@@ -4,18 +4,20 @@ from skopt.learning import GaussianProcessRegressor
 from skopt.learning.gaussian_process.kernels import ConstantKernel
 from skopt.learning.gaussian_process.kernels import Matern, WhiteKernel
 
-NRANDOM = 3
-NTOTAL = 10
 
-cov_amplitude = ConstantKernel(1.0, (0.01, 5.0))
+
+#cov_amplitude = ConstantKernel(1.0, (0.01, 5.0))
+cov_amplitude = ConstantKernel(1.0, "fixed")
 
 other_kernel = Matern(
     length_scale=np.ones(1),
-    length_scale_bounds=[(0.01, 100)],
+    length_scale_bounds=[(0.3, 10)],
     nu=2.5)
 
+white_kernel = WhiteKernel()
+
 gp = GaussianProcessRegressor(
-    kernel=cov_amplitude * other_kernel + WhiteKernel(),
+    kernel=cov_amplitude * other_kernel + white_kernel,
     normalize_y=True, alpha=0.0, noise=10e-7,
     n_restarts_optimizer=2)
 
